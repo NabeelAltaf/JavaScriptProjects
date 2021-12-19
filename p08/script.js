0a07815b0843cf7060ee716477b74f262fee568c
NabeelAltaf <nabeelaltaf1@outlook.com>
2021-12-19 17:12:07 -0500

    Fixing getMealByID function
    Trying to fix geatMealByID

--- p08/script.js
+++ p08/script.js
@@ -27,7 +27,7 @@ function searchMeal (e) {
                 console.log(data);
                 resultHeading.innerHTML = `<h2>Search results for '${term}'</h2>`;
                 if (data.meals == null) {
-                    resultHeading.innerHTML = `<p>No results found for '${term}'. Please try a different serach.<p>`
+                    resultHeading.innerHTML = `<p>No results found for '${term}'. Please try a different search.<p>`
                 } else {
                     mealContainer.innerHTML = data.meals.map( meal => `
                         <div class = "meal">
@@ -37,7 +37,7 @@ function searchMeal (e) {
                             </div>
                         </div>
                     `)
-                    .join('');
+                    .join(''); // Will get rid of the commas
                 }
 
             })
@@ -45,6 +45,18 @@ function searchMeal (e) {
         alert('ERROR: Please enter a valid search.');
     }
 
+    // Clear the search term
+    search.value = '';
+
+}
+
+// Function to fetch meal data using the Meal ID
+function getMealByID (mealID) {
+    fetch (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then (res => res.json())
+    .then (data => {
+        console.log(data);
+    })
 }
 
 
@@ -55,5 +67,26 @@ function searchMeal (e) {
 // Event Listener
     // 1. Submit
     submit.addEventListener('submit', searchMeal);
+
+    // 2. Upon clicking a meal, instructions will be displayed for the recipe
+    mealContainer.addEventListener('click', e => {
+        // The first thing we need to do is figure out what is being clicked
+        const mealInfo = e.path.find( item => {
+            // This function shows the full stack, HTMl down to the meal info. We need to add a filter
+            if (item.classList) {
+                return item.classList.contains('meal-info');
+            } else {
+                return false
+            }
+        });
+
+        if (mealInfo) {
+            const mealID = mealInfo.getAttribute('data-mealID');
+            console.log(mealInfo);
+            console.log(mealID);
+            getMealByID(mealID);
+        }
+
+});
 
     // stopping at 1:01:53
